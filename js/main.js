42e2557d4c69c76e4743d8d0f2cfe3b5c29403f8
Barrbados <yashick@bk.ru>
2021-10-19 23:43:09 +0300

    Выполнил дз из модуль  4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,62 @@
+const NAMES = [
+  'Маша',
+  'Петя',
+  'Савелий',
+  'Катя',
+  'Кристина',
+  'Денис',
+  'Леонид',
+  'Иван',
+  'Светлана',
+  'Павел',
+  'Ирина',
+  'Юлия',
+  'Дарья',
+  'Анастасия',
+];
+
+//description, строка — описание фотографии. Описание придумайте самостоятельно.
+const DESCRIPTIONS = [
+  'Отличный был день',
+  'Это я вчера',
+  'Очень красиво',
+  'Лайкай меня',
+  'Как Бузова',
+];
+
+//вам необходимо взять одно или два случайных предложения из представленных ниже:
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
 // Получение случайного целого числа в заданном интервале, включительно
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-getRandomIntInclusive (10, 100);
 
 //подсчет длинны строки
 function calcLenghtString(str, maxlenght) {
   return str.lenght <= maxlenght;
 }
 calcLenghtString(0, 140);
+
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const createUser = (id) => ({
+  id,
+  name: getRandomArrayElement(NAMES),
+  avatar: `img/avatar-${  getRandomIntInclusive (1, 6) }.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  likes: getRandomIntInclusive (15, 200),
+  description: getRandomArrayElement(DESCRIPTIONS),
+});
+
+
+const similarUsers = Array.from({length: 25}, ( val, ind ) => createUser( ind + 1 ));
