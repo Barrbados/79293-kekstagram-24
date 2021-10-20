7a7555843a68789ebf3363b38239f5ea353a45b7
Barrbados <yashick@bk.ru>
2021-10-20 22:52:05 +0300

    исправил замечания

--- js/main.js
+++ js/main.js
@@ -31,32 +31,38 @@ const MESSAGES = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!',
 ];
 
 // Получение случайного целого числа в заданном интервале, включительно
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+};
 
 //подсчет длинны строки
-function calcLenghtString(str, maxlenght) {
-  return str.lenght <= maxlenght;
-}
+const calcLenghtString = (str, maxlenght) => str.lenght <= maxlenght;
 calcLenghtString(0, 140);
 
 const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+const getComments = (id) => ({
+  id,
+  name: getRandomArrayElement(NAMES),
+  avatar: `img/avatar-${getRandomIntInclusive (1, 6)}.svg`,
+  message: getRandomIntInclusive(0, 1)
+    ? `${getRandomArrayElement(MESSAGES)} ${getRandomArrayElement(MESSAGES)}`
+    : getRandomArrayElement(MESSAGES),
+});
 
-const createUser = (id) => ({
+const getPhoto = (id) => ({
   id,
   name: getRandomArrayElement(NAMES),
   avatar: `img/avatar-${  getRandomIntInclusive (1, 6) }.svg`,
-  message: getRandomArrayElement(MESSAGES),
+  comments:  Array.from({length: getRandomIntInclusive(1, 3)}, ( val, ind ) => getComments( ind + 1 )),
   likes: getRandomIntInclusive (15, 200),
   description: getRandomArrayElement(DESCRIPTIONS),
 });
 
-
-const similarUsers = Array.from({length: 25}, ( val, ind ) => createUser( ind + 1 ));
+const getUserPhots = () => Array.from({length: 25}, ( val, ind ) => getPhoto( ind + 1 ));
