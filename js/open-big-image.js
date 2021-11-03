6844164a4a34c6095dbc5dfd624e65dde67e031f
Barrbados <yashick@bk.ru>
2021-11-03 15:52:50 +0300

    прикрутил фотку из превью к биг имадж

--- js/open-big-image.js
+++ js/open-big-image.js
@@ -5,18 +5,49 @@ export const fn2 = () => {
   const commentsLoader = document.querySelector('.comments-loader');
   const trumbNails = document.querySelectorAll('.picture__img');
   const bigPicture = document.querySelector('.big-picture');
+  const bigPictureImg = document.querySelector('.big-picture__img');
   const bodyElement = document.querySelector('body');
   const bigpictureCancel = document.querySelector('.big-picture__cancel');
 
-  console.log('trumbNails', trumbNails);
+
   trumbNails.forEach((elm) => {
 
     elm.addEventListener('click', (evt) => {
       evt.preventDefault();
-      console.log('evt.target', evt.target.id);
       const targetId = evt.target.id;
-      const dataItem = data.find((val) => `${val.id}` === targetId);
-      console.log('data', data);
+      const dataItem = data.find((val) => `${val.id}` === targetId);//с этим мне помогли
+      const img = bigPicture.querySelector('img');
+      const likesСount = bigPicture.querySelector('.likes-count');
+      const commentsСount = bigPicture.querySelector('.comments-count');
+      const socialPicture = bigPicture.querySelector('.social__picture');
+      
+      // const dataItemComments = dataItem(comments).find((val) => `${val.id}` === targetId);
+      //?????Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments
+      //  <li class="social__comment">
+      //     <img
+      //         class="social__picture"
+      //         src="{{аватар}}"
+      //         alt="{{имя комментатора}}"
+      //         width="35" height="35">
+      //     <p class="social__text">{{текст комментария}}</p>
+      //  </li>
+      const socialComments = bigPicture.querySelector('.social__comments');
+
+      //Описание фотографии description вставьте строкой в блок .social__caption
+      const socialCaption = bigPicture.querySelector('.social__caption');
+
+      img.src = dataItem.avatar;
+      likesСount.textContent = dataItem.likes;
+      commentsСount.textContent = dataItem.comments.length;
+      socialComments.value = dataItem.comments.length;
+      socialCaption.textContent = dataItem.description;
+      socialPicture.src = dataItem.avatar;
+      // socialComments.src = dataItemComments.avatar;
+
+
+
+
+
 
       console.log('dataItem', dataItem);
       bigPicture.classList.remove('hidden');
